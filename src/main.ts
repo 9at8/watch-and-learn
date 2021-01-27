85cc242aac23f4c8bf89b99eae4833de04953c40
Aditya Thakral <github@9at8.dev>
2021-01-27 05:11:35 -0500

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import videojs from 'video.js'
+
+function exponentialWait(
+  check: () => boolean,
+  limit: number = 10000,
+  initialTimeout = 100
+) {
+  return new Promise<void>((resolve, reject) => {
+    function wait(timeout: number) {
+      setTimeout(() => {
+        if (check()) {
+          resolve();
+        } else if (timeout > limit) {
+          reject(`Timelimit "${limit}" exceeded`);
+        } else {
+          wait(timeout * 2);
+        }
+      }, timeout);
+    }
+
+    wait(initialTimeout);
+  });
+}
+
+const DEFAULT_PLAYER_SELECTOR = "d2l-labs-media-player";
+
+async function main() {
+  const getPlayer = () => document.querySelector(DEFAULT_PLAYER_SELECTOR);
+  await exponentialWait(() => getPlayer() != null, 10000, 50);
+
+  const oldPlayer = getPlayer();
+
+  console.log(oldPlayer);
+}
+
+main();
